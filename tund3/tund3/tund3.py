5e47ad18792a0acbf8e04147978f8270e13a240c
Nikita Nikiforov <130934938+Polopo232@users.noreply.github.com>
2025-02-14 14:47:29 +0200

    tund3.py

--- tund3/tund3/tund3.py
+++ tund3/tund3/tund3.py
@@ -1,15 +1,88 @@
 
 from random import *
-#naidis 4
+#naidis 14
+
+    
+
+#naidis 9
+
+tsekk=0
+
+choice2 = input("Kas sa tahad osta piim?(y/n): ")
+if choice2 == "y":
+    palju = int(input("Kui palju?: "))
+    tsekk = tsekk + palju * 0.8
+choice2 = input("Kas sa tahad osta leib?(y/n): ")
+if choice2 == "y":
+    palju = int(input("Kui palju?: "))
+    tsekk = tsekk + palju * 1.2
+choice2 = input("Kas sa tahad osta juust?(y/n): ")
+if choice2 == "y":
+    palju = int(input("Kui palju?: "))
+    tsekk = tsekk + palju * 2
+
+print(tsekk)
+    
 
 
-n1 = str(input("esimese nimi: "))
-n2 = str(input("teise nimi: "))
+#naidis 8
 
-if n1.lower() == n2.lower():
-        print(f"{n1} ja {n2} pinginaabrid")
+pikk = float(input("Kuidas sinu pikk?: "))
+if pikk <= 120:
+    print("sa oled goblin")
+elif pikk <= 160:
+    print("sa oled keskmine")
+elif pikk >= 180:
+    print("Sa oled hiiglane")
 else:
-        print(f"{n1} ja {n2} ei ole pinginaabrid.")
+    print("Midagi läks valesti")
+
+
+#naidis 7
+
+temp = float(input("Kuidas temp?: "))
+if temp <= 18:
+    print("Soovitav")
+else:
+    print("Soovitav toasoojus talvel")
+
+
+#naidis 6
+price1 = float(input("Kuidas hinna?: "))
+
+if price1 >= 700:
+    price1 = price1 * 0.7
+    print(price1)
+else:
+    print("ei")
+
+#naidis 5
+wall1 = float(input("esimene sein: "))
+wall2 = float(input("teine sein: "))
+
+Sfloor = wall1 * wall2
+print(f"{Sfloor} m2")
+choice = input("Kas sa tahad remontide teha?(y/n): ")
+if choice.lower() == "y":
+    price = float(input("Kui palju maksab 1 ruutmeeter?: "))
+    price *= Sfloor
+    print(price)
+elif choice.lower() == "n":
+    print("Hästi")
+else:
+    print("Midagi läks valesti")
+
+
+
+
+#naidis 4
+n1 = input("esimese nimi: ")
+n2 = input("teise nimi: ")
+
+if n1.isalpha() and n2.isalpha() and n1 == "nikita" and n2 == "artjom":
+    print(f"{n1} ja {n2} pinginaabrid")
+else:
+    print(f"{n1} ja {n2} ei ole pinginaabrid.")
 
 #naidis 3
 # nimi=input("Mis on sinu nimi?: ")
