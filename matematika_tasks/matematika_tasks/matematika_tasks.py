4ecc296f3a1c961a8810a316ec674c241277729e
Nikita Nikiforov <130934938+Polopo232@users.noreply.github.com>
2025-02-26 15:38:32 +0200

    Add files via upload

---
+++ matematika_tasks/matematika_tasks/matematika_tasks.py
@@ -0,0 +1,114 @@
+import random
+
+op = ["+", "-"]
+op1 = ["+", "-", "*", "/"]
+op2 = ["+", "-", "*", "/", "**"]
+
+while True:
+    try:
+        choice1 = int(input("Tase (1, 2, 3):  "))
+    except:
+        print("Palun sisestage number")
+
+    if choice1 == 1:
+        oige = 0
+        for x in range(1, 11):
+            try:
+                num1 = random.randint(1, 50)
+                num2 = random.randint(1, 50)
+                random_op = random.choice(op)
+                ans = int(input(f"{num1} {random_op} {num2} = "))
+                correct_answer = eval(f"{num1} {random_op} {num2}")
+                if ans == correct_answer:
+                    print("Õige vastus")
+                    oige += 1
+                else:
+                    print("Vale vastus")
+            except:
+                print("Palun sisestage number")
+
+        oige_percentage = (oige / 10) * 100
+        print(f"Õigeid vastuseid: {oige}")
+        print(f"Hinne: {oige_percentage}%")
+        if oige_percentage >= 90:
+            print("See on 5")
+        elif oige_percentage >= 75:
+            print("See on 4")
+        elif oige_percentage >= 60:
+            print("See on 3")
+        elif oige_percentage >= 50:
+            print("See on 2")
+        else:
+            print("See on 1")
+        choice_q = input("Kas sa jätkama? (y/n): ")
+        if choice_q == "n":
+            break
+
+
+
+
+    if choice1 == 2:
+        oige = 0
+        for x in range(1, 11):
+            try:
+                num1 = random.randint(1, 100)
+                num2 = random.randint(1, 100)
+                random_op = random.choice(op1)
+                ans = int(input(f"{num1} {random_op} {num2} = "))
+                if ans == eval(str(num1) + random_op + str(num2)):
+                    print("Õige vastus")
+                    oige += 1
+                else:
+                    print("Vale vastus")
+            except:
+                print("Palun sisestage number")
+
+        oige_percentage = (oige / 10) * 100
+        print(f"Õigeid vastuseid: {oige}")
+        print(f"Hinne: {oige_percentage}%")
+        if oige_percentage >= 90:
+            print("See on 5")
+        elif oige_percentage >= 75:
+            print("See on 4")
+        elif oige_percentage >= 60:
+            print("See on 3")
+        elif oige_percentage >= 50:
+            print("See on 2")
+        else:
+            print("See on 1")
+        choice_q = input("Kas sa jätkama? (y/n): ")
+        if choice_q == "n":
+            break
+
+    if choice1 == 3:
+        oige = 0
+        for x in range(1, 11):
+            try:
+                num1 = random.randint(1, 150)
+                num2 = random.randint(1, 150)
+                random_op = random.choice(op2)
+                ans = int(input(f"{num1} {random_op} {num2} = "))
+                if ans == eval(str(num1) + random_op + str(num2)):
+                    print("Õige vastus")
+                    oige += 1
+                else:
+                    print("Vale vastus")
+            except:
+                print("Palun sisestage number")
+
+        oige_percentage = (oige / 10) * 100
+        print(f"Õigeid vastuseid: {oige}")
+        print(f"Hinne: {oige_percentage}%")
+        if oige_percentage >= 90:
+            print("See on 5")
+        elif oige_percentage >= 75:
+            print("See on 4")
+        elif oige_percentage >= 60:
+            print("See on 3")
+        elif oige_percentage >= 50:
+            print("See on 2")
+        else:
+            print("See on 1")
+        choice_q = input("Kas sa jätkama? (y/n): ")
+        if choice_q == "n":
+            break
