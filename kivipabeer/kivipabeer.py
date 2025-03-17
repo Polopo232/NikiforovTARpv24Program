bf54081af35d14e67befdc7b8415ff833b939650
Nikita Nikiforov <130934938+Polopo232@users.noreply.github.com>
2025-03-17 09:46:13 +0200

    Add files via upload

---
+++ kivipabeer/kivipabeer.py
@@ -0,0 +1,22 @@
+﻿
+import random
+
+while True:
+    choices = ['kivi', 'käärid', 'paber']
+    print("Valige: kivi, käärid või paber")
+    try:
+        user_choice = input().lower()
+    except:
+        print("Viga !")
+
+    computer_choice = random.choice(choices)
+    print(f"Arvuti on valinud: {computer_choice}")
+
+    if user_choice == computer_choice:
+        print("Viik !")
+    elif (user_choice == 'kivi' and computer_choice == 'käärid') or \
+         (user_choice == 'käärid' and computer_choice == 'paber') or \
+         (user_choice == 'paber' and computer_choice == 'kivi'):
+        print("Sa oled võitnud!")
+    else:
+        print("Sa kaotasid!")
